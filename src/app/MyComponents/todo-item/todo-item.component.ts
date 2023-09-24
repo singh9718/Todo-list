0017602f9cfc96298cd4702dd67cbc4ab0ec1994
Roshan Singh <singh.roshan9718@gmail.com>
2023-09-24 13:50:00 +0530

    added http requests

--- src/app/MyComponents/todo-item/todo-item.component.ts
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Todo } from 'src/app/Todo';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-todo-item',
@@ -9,20 +11,33 @@ import { Todo } from 'src/app/Todo';
 export class TodoItemComponent implements OnInit {
 
   @Input() todo!: Todo;
-  @Output() todoDelete:EventEmitter<Todo>=new EventEmitter();
-  @Output() todoCheckbox:EventEmitter<Todo>=new EventEmitter();
+  @Output() todoDelete: EventEmitter<Todo> = new EventEmitter();
+  @Output() todoCheckbox: EventEmitter<Todo> = new EventEmitter();
 
-  constructor(){}
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    
+
   }
 
-  onClick(todo:Todo){
-    this.todoDelete.emit(todo);
+  onClick(todo: Todo) {
+    // console.log('Received todo object:', todo);
+    if (todo._id) {
+      this.http.delete(`http://localhost:3000/api/v1/deleteTodo/${todo._id}`).subscribe(
+        (response) => {
+          // console.log('Delete response:', response);
+          this.todoDelete.emit(todo);
+        },
+        (error) => {
+          console.error('Delete error:', error);
+        }
+      );
+    } else {
+      console.error('Todo does not have a valid _id property.');
+    }
   }
 
-  onCheckboxClick(todo: any){
+  onCheckboxClick(todo: Todo) {
     this.todoCheckbox.emit(todo);
   }
 }
